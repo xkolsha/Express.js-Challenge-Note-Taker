1d8a11994827ea6f7d0b6eae2af934beecbea53c
xkolsha <aviad85@outlook.com>
2023-07-16 01:51:24 -0300

    added const fs and uudi

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
