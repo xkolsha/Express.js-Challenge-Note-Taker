c16b641e650cd8f4288288e2797ecd0ab190d22d
xkolsha <aviad85@outlook.com>
2023-07-16 01:25:14 -0300

    added sever.js and install npm packages

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
