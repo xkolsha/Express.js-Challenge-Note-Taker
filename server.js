da94452debf5f43bbd37eb25e4f9b1cc50ab1fa0
xkolsha <aviad85@outlook.com>
2023-07-16 03:12:36 -0300

    added comments + use for public + get notes

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+// Import necessary packages
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
+// Create an instance of an Express.js app
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "./public")));
 
+// Define the port the server should listen on
+const PORT = process.env.PORT || 3000;
+
+// Get all notes
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+// Start the server
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
