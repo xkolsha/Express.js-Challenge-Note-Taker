3c84dbe96437060d38e1d0b6947f144ad5ead07c
xkolsha <aviad85@outlook.com>
2023-07-16 22:36:35 -0300

    added req to import notes + post for new note

--- server.js
+++ server.js
@@ -13,9 +13,21 @@ app.use(express.static(path.join(__dirname, "./public")));
 // Define the port the server should listen on
 const PORT = process.env.PORT || 3000;
 
+// Import the existing notes from db.json
+let notes = require("./db/db.json");
+
 // Get all notes
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+// Add a new note
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  notes.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(newNote);
 });
 
 // Start the server
